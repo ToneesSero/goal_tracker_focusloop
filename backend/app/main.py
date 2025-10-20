9ff77e239bdaf4825bf934400b2e7d201ed35acd
ToneesSero <danila_vasilev_2000@list.ru>
2025-10-20 16:43:59 +0300

    allow cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="GoalTracker API", version="1.0.0")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
